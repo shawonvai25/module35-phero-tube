7293bb6e1af7ac907c6d4625805f3007ac4897c4
shawonvai25 <abdullahalshawon25@gmail.com>
2025-01-14 21:06:05 +0600

    add

--- script/video.js
+++ script/video.js
@@ -1,36 +1,107 @@
-console.log('video script added')
 
-// 1 Fetch , load and show categories on html
+
+// 1. Fetch , load and show categories on HTML
 
 // create loadCategories
-const loadCategories = () =>{
+const loadCategories = () => {
 
-    //fetch the data
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')    
-    .then((res) => res.json())
-    .then(data => displayCategories(data.categories))
-    .catch(error => console.log(error))
+  //  fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+  .then((res) => res.json())
+  .then(data => displayCategories(data.categories))
+  .catch((error) => console.log(error));
+};
+
+const loadVideos = () => {
+  // fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+  .then((res) => res.json())
+  .then((data) => displayVideos(data.videos) )
+  .catch((error) => console.log(error))
 }
 
-// create displayCategories
-const displayCategories = (categories) => {
+// const cardDemo = {
+//   "category_id": "1001",
+//   "video_id": "aaaa",
+//   "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//   "title": "Shape of You",
+//   "authors": [
+//       {
+//           "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//           "profile_name": "Olivia Mitchell",
+//           "verified": ""
+//       }
+//   ],
+//   "others": {
+//       "views": "100K",
+//       "posted_date": "16278"
+//   },
+//   "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// };
 
-    const categoryContainer = document.getElementById("categories");
 
-     categories.forEach( (item) =>{
-         console.log(item);
+const displayVideos = (videos) => {
+   const videoContainer = document.getElementById('videos');
+  videos.forEach(video => {
+    console.log(video)
 
-    //create a button
+    const card = document.createElement("div");
+    card.classList = "card card-compact"
+    card.innerHTML = `
+    <figure class=" h-[200px] rounded-md relative"> 
+    <img
+    class="h-full w-full object-cover"
+      src=${video.thumbnail}
+      alt="Shoes" />
 
-    const button = document.createElement('button');
-    button.classList = "btn";
-    button.innerText = item.category;
-    
-    // add button to category container
-    categoryContainer.append(button);
+      ${
+        video.others.posted_date?.length == 0 ? "" : `    <span class="absolute text-white bg-black right-2 bottom-2 rounded p-1">${video.others.posted_date}</span>`
+      }
+  
+  </figure>
+  <div class="px-0 py-2 flex gap-2  ">
+   <div>
+   <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+   </div>
+   <div>
+   <h2 class="font-semibold">${video.title}</h2>
+   <div class="flex items-center gap-2">
+   <p class="text-gray-400">${video.authors[0].profile_name}</p> 
+   
+   ${video.authors[0].verified == true ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />` : ""}
 
-     });
+   </div>
+   
+   <p></p>
+   </div>
+  </div>
+  `;
+  videoContainer.append(card);
+  });
+
 };
 
 
-loadCategories();
+// create DisplayCategories
+const displayCategories = (categories) => {   
+   
+    const categoryContainer = document.getElementById("categories");
+  
+
+    categories.forEach((item) => {
+      console.log(item);
+
+      // create button
+      
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    // add button to category container
+    categoryContainer.append(button);
+
+    });
+ };
+
+ loadCategories();
+ loadVideos();
